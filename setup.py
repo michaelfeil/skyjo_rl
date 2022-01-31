83baea778c4a799b19376f941d83790e5f89188d
Michael Feil <michaelfeil@example.com>
2022-01-31 11:12:20 +0100

    update pipeline and setup

--- setup.py
+++ setup.py
@@ -1,10 +1,24 @@
 from setuptools import find_packages, setup
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="rlskyjo",
     packages=find_packages(),
-    version="0.1.0",
+    version="0.0.1",
     description="A RL repo for playing the card game SkyJo",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="Michael Feil",
     license="MIT",
+    url="https://github.com/michaelfeil/skyjo_rl",
+    project_urls={
+        "Bug Tracker": "https://github.com/michaelfeil/skyjo_rl/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
