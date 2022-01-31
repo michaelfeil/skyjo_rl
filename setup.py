eded05842eaef24c1f836b293fc3747fb1489322
Michael Feil <michaelfeil@example.com>
2022-01-31 11:34:46 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,13 @@ from setuptools import find_packages, setup
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
+
 setup(
     name="rlskyjo",
     packages=find_packages(),
     version="0.0.1",
-    description="A RL repo for playing the card game SkyJo",
+    description="Multi-Agent Reinforcement Learning Environment"
+    " for the card game SkyJo, compatible with PettingZoo and RLLIB",
     long_description=long_description,
     long_description_content_type="text/markdown",
     author="Michael Feil",
